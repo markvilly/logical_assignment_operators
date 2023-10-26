9bf48ca70f9a4e0b6aeb18b384d902d9b148dfe5
mark.paul <markpaulbusiness66@gmail.com>
2023-10-26 21:46:24 +0300

    Object Literals three Types

--- script.js
+++ script.js
@@ -5,6 +5,24 @@ const flights =
   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
 // Data needed for first part of the section
+
+const weekDays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+const hours = {
+  [weekDays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekDays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [`day-${2 + 4}`]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: "Classico Italiano",
   location: "Via Angelo Tavanti 23, Firenze, Italy",
@@ -12,11 +30,14 @@ const restaurant = {
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
 
-  order: function (starterIndex, mainIndex) {
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+  //ES6 enhance object literals
+  hours,
 
-  orderDelivery: function ({
+  //ES6 METHOD
+  orderDelivery({
     time = "20:00",
     address = "",
     mainIndex = 1,
@@ -26,29 +47,14 @@ const restaurant = {
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} by ${time}`
     );
   },
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
-  orderPizzas: function (mainIngredients, ...otherIngredients) {
+  orderPizzas(mainIngredients, ...otherIngredients) {
     console.log(mainIngredients);
     console.log(otherIngredients);
-  },
-
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
   },
 };
 
@@ -85,3 +91,5 @@ console.log("------FOR OF LOOP-----");
 // }
 
 for (const [i, el] of menu.entries()) console.log(`${i + 1}: ${el}`);
+
+//ENHANCED OBJECT LITERALS.
