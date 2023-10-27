15ac5206c7d649d85802e3a5c6237a926a393c67
mark.paul <markpaulbusiness66@gmail.com>
2023-10-27 10:47:03 +0300

    LOOPING OBJECT KEYS AND VALUES

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const flights =
 
 const weekDays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
-const hours = {
+const openingHours = {
   [weekDays[3]]: {
     open: 12,
     close: 22,
@@ -17,7 +17,7 @@ const hours = {
     open: 11,
     close: 23,
   },
-  [`day-${2 + 4}`]: {
+  [weekDays[5]]: {
     open: 0, // Open 24 hours
     close: 24,
   },
@@ -34,7 +34,7 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
   //ES6 enhance object literals
-  hours,
+  openingHours,
 
   //ES6 METHOD
   orderDelivery({
@@ -93,3 +93,91 @@ console.log("------FOR OF LOOP-----");
 for (const [i, el] of menu.entries()) console.log(`${i + 1}: ${el}`);
 
 //ENHANCED OBJECT LITERALS.
+// there are of three types. objects can be declared globally, methods can be declared without the function keyword, global arrays can be used in the object.
+
+//OPTIONAL CHAINING
+
+// restaurant.hours.fri && console.log(restaurant.hours.fri.open);
+
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+//with OPTIONAL CHAINING
+
+// console.log(restaurant.openingHours.mon.open);
+
+console.log(restaurant.openingHours.fri?.open);
+console.log(restaurant.openingHour?.mon?.open);
+
+const user = {
+  name: "John",
+  address: {
+    street: "123 Main St",
+    city: "Exampleville",
+  },
+};
+
+const city = user.address && user.address.city;
+
+const cityWithOptionalChaining = user.address?.city;
+
+console.log(cityWithOptionalChaining);
+
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+for (const day of days) {
+  const weOpen = "we are open at ";
+  const opens =
+    (weOpen && restaurant.openingHours[day]?.open) ?? "we are closed";
+
+  console.log(`on ${day}, ${opens}`);
+}
+
+// ON METHODS.
+
+console.log(restaurant.order?.(0, 1) ?? "Method does not exist");
+
+console.log(restaurant.orderAlPaccino?.(2, 3) ?? "Method does not exist");
+
+//ON ARRAYS
+const users1 = [];
+const users = [{ name: "Jonas", email: "hello@jonas.com" }];
+
+console.log(users[0]?.name ?? "User array empty.");
+
+console.log(users1[0]?.name ?? "User array empty");
+
+weekDays.forEach((element) => {
+  console.log(element);
+});
+
+for (const [i, el] of weekDays.entries()) console.log(`${i + 1}: ${el}`);
+
+// LOOPING OVER OBJECTS
+console.log("------LOOPING OVER OBJECTS KEYS-----");
+
+const properties = Object.keys(openingHours);
+
+let openStr = `we are open on ${properties.length} days: `;
+
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+
+console.log(openStr);
+
+console.log("-----OBJECT VALUES-----");
+
+const values = Object.values(openingHours);
+console.log(values);
+
+// Entries forobjects.
+
+console.log("------Entries for Objects-----");
+
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for (const [day, { open: o, close: c }] of entries) {
+  console.log(`On ${day} we are open at ${o} and close at ${c}`);
+}
