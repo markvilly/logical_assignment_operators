6b7432e3d4ac256b28a519f7efad3693fda797ab
mark.paul <markpaulbusiness66@gmail.com>
2023-10-31 02:09:06 +0300

    PADDING WITH STRINGS

--- script.js
+++ script.js
@@ -676,10 +676,34 @@ const capitalizeName = function (name) {
   const newNames = [];
 
   for (const n of names) {
-    newNames.push(n[0].toUpperCase() + n.slice(1));
+    // newNames.push(n[0].toUpperCase() + n.slice(1));
+
+    newNames.push(n.replace(n[0], n[0].toUpperCase(), ""));
   }
   console.log(newNames.join(" "));
 };
 
-capitalizeName("Jessica ann smith davis");
+capitalizeName("jessica ann smith davis");
 capitalizeName("jonas schmedtmann");
+
+// padding
+
+const message = "Go to gate 23!";
+console.log(message.padStart(10, "*").padEnd(35));
+console.log("Jonas".padStart(13, "*"));
+
+// APPLICATIONS OF PADDING
+
+const maskCredid = function (num) {
+  // convert number to string
+  const str = num + "";
+  //getting the last 4 digits.
+  const last = str.slice(-4);
+
+  console.log(last.padStart(str.length, "*"));
+};
+
+maskCredid(3322333);
+maskCredid("24243245454");
+
+console.log("jonasey".padStart(10, "X"));
